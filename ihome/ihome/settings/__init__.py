4a975f97214653acf446d2025d2ff1676a7d66a6
lc <letsplaysomedota2@163.com>
2020-10-20 14:10:14 +0800

    ihome搭建完成

---
+++ ihome/ihome/settings/__init__.py
@@ -0,0 +1,5 @@
+#-*- codeing = utf-8 -*-
+#@Author: lc
+#@Software: PyCharm
+#@File: __init__.py.py
+#@Time: 2020/10/10 下午4:43
---
+++ ihome/ihome/settings/prod.py
@@ -0,0 +1,5 @@
+#-*- codeing = utf-8 -*-
+#@Author: lc
+#@Software: PyCharm
+#@File: prod.py
+#@Time: 2020/10/10 下午4:44
